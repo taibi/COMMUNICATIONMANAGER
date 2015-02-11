c32d163e8419f23f71b6e01b9911b4bfb9e3e699
taibi <t.khachani@gmail.com>
2015-02-10 22:34:21 -0500

    second commit

--- communication_manager/CommunicationManager.cs
+++ communication_manager/CommunicationManager.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Drawing;
 namespace communication_manager
 {
     class CommunicationManager
     {
+        
     }
 }
